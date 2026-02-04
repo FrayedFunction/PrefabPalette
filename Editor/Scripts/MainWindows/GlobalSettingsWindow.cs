0e5793f3f93bba71a86ced1b2effb0d86d0448b0
Jon McCreadie <mccducky23@gmail.com>
2026-02-04 13:33:23 +0000

    Squashed commit of the following:
    commit 94beb1343761b9e7db2c9aad85fafd291721ef87
    Author: Jon McCreadie <mccducky23@gmail.com>
    Date:   Wed Feb 4 13:32:37 2026 +0000
    
        Replace EditorGUI.indentLevel with Helpers.IndentBlock
    
    commit 096d8021edd530dda2d8f7422a992fce2b030c45
    Author: Jon McCreadie <mccducky23@gmail.com>
    Date:   Tue Feb 3 23:56:06 2026 +0000
    
        update settings window
    
        - Add contact and links section.
        - Add indent block helper method.
        - Add link label helper.

--- Editor/Scripts/MainWindows/GlobalSettingsWindow.cs
+++ Editor/Scripts/MainWindows/GlobalSettingsWindow.cs
@@ -24,65 +24,91 @@ namespace PrefabPalette
         private void OnGUI() 
         {
             scrollPos = GUILayout.BeginScrollView(scrollPos);
-
+            GUILayout.Label("v1.0.3", EditorStyles.miniLabel);
+            
             Helpers.TitleText("Prefab Palette: Settings");
             Helpers.DrawLine(Color.gray);
 
             // Palette Settings
             GUILayout.Label("Palette:", EditorStyles.whiteLargeLabel);
-            EditorGUI.indentLevel++;
-            Settings.palette_gridColumns = Mathf.Max(1, EditorGUILayout.IntField("Columns", Settings.palette_gridColumns));
-            Settings.palette_minThumbnailScale = Mathf.Clamp(EditorGUILayout.FloatField("Min Thumbnail Scale", Settings.palette_minThumbnailScale), 50f, Settings.palette_maxThumbnailScale);
-            Settings.palette_maxThumbnailScale = Mathf.Clamp(EditorGUILayout.FloatField("Max Thumbnail Scale", Settings.palette_maxThumbnailScale), Settings.palette_minThumbnailScale, 500f);
-            EditorGUI.indentLevel--;
-            GUILayout.Space(4);
-            Helpers.DrawLine(Color.gray);
-            GUILayout.Space(4);
+            Helpers.IndentBlock(1, () =>
+            {
+                Settings.palette_gridColumns = Mathf.Max(1, EditorGUILayout.IntField("Columns", Settings.palette_gridColumns));
+                Settings.palette_minThumbnailScale = Mathf.Clamp(EditorGUILayout.FloatField("Min Thumbnail Scale", Settings.palette_minThumbnailScale), 50f, Settings.palette_maxThumbnailScale);
+                Settings.palette_maxThumbnailScale = Mathf.Clamp(EditorGUILayout.FloatField("Max Thumbnail Scale", Settings.palette_maxThumbnailScale), Settings.palette_minThumbnailScale, 500f);
+            });
+
+            Helpers.Line(Color.gray);
 
             // Placer Setttings
             GUILayout.Label("Placer:", EditorStyles.whiteLargeLabel);
-            EditorGUI.indentLevel++;
-            Settings.placer_includeMask = LayerMaskField("Include Layers", Settings.placer_includeMask);
-            Settings.placer_color = EditorGUILayout.ColorField("Color", Settings.placer_color);
-            Settings.placer_radius = Mathf.Max(0.01f, EditorGUILayout.FloatField("Radius", Settings.placer_radius));
-            Settings.placer_mouseMoveThreshold = (Mathf.Max(0.001f, EditorGUILayout.FloatField("Mouse Move Threshold", Settings.placer_mouseMoveThreshold)));
-            Settings.placer_maxRaycastDistance = (Mathf.Max(1f, EditorGUILayout.FloatField("Raycast Distance", Settings.placer_maxRaycastDistance)));
-            EditorGUI.indentLevel-- ;
-            GUILayout.Space(4);
-            Helpers.DrawLine(Color.gray);
-            GUILayout.Space(4);
+            Helpers.IndentBlock(1, () =>
+            {
+                Settings.placer_includeMask = LayerMaskField("Include Layers", Settings.placer_includeMask);
+                Settings.placer_color = EditorGUILayout.ColorField("Color", Settings.placer_color);
+                Settings.placer_radius = Mathf.Max(0.01f, EditorGUILayout.FloatField("Radius", Settings.placer_radius));
+                Settings.placer_mouseMoveThreshold = (Mathf.Max(0.001f, EditorGUILayout.FloatField("Mouse Move Threshold", Settings.placer_mouseMoveThreshold)));
+                Settings.placer_maxRaycastDistance = (Mathf.Max(1f, EditorGUILayout.FloatField("Raycast Distance", Settings.placer_maxRaycastDistance)));
+            });
+
+            Helpers.Line(Color.gray);
 
             // Overlay Settings
             GUILayout.Label("Overlay:", EditorStyles.whiteLargeLabel);
-            EditorGUI.indentLevel++;
-            Settings.overlay_autoSize = EditorGUILayout.Toggle("Auto Size?", Settings.overlay_autoSize);
-            Settings.overlay_size = Settings.overlay_autoSize ? Vector2.zero : EditorGUILayout.Vector2Field("Size", Settings.overlay_size);
-            EditorGUI.indentLevel--;
-            GUILayout.Space(4);
-            Helpers.DrawLine(Color.gray);
-            GUILayout.Space(4);
-            
+            Helpers.IndentBlock(1, () =>
+            {
+                Settings.overlay_autoSize = EditorGUILayout.Toggle("Auto Size?", Settings.overlay_autoSize);
+                Settings.overlay_size = Settings.overlay_autoSize ? Vector2.zero : EditorGUILayout.Vector2Field("Size", Settings.overlay_size);
+            });
+
+            Helpers.Line(Color.gray);
+
             // Window Scale
             GUILayout.Label("Window Scale:", EditorStyles.whiteLargeLabel);
-            EditorGUI.indentLevel++;
-            Settings.globalMinWindowScale = EditorGUILayout.Vector2Field("Global min", Settings.globalMinWindowScale);
-            // Only min needs to be clamped to a floor as max floor is already clamped by min.
-            Settings.globalMinWindowScale = Vector2.Max(Settings.globalMinWindowScale, Vector2.one);
-            Settings.globalMaxWindowScale = EditorGUILayout.Vector2Field("Global max", Settings.globalMaxWindowScale);
-            Settings.globalMaxWindowScale = Vector2.Max(Settings.globalMaxWindowScale, Settings.globalMinWindowScale);
-            GUILayout.Space(2);
-            Helpers.IndentedLabel("Palette", EditorGUI.indentLevel, EditorStyles.whiteLabel);
-            WindowScaleSettingsGUI.Draw(Settings.paletteWindowScale);
-            GUILayout.Space(1);
-            Helpers.IndentedLabel("Collections Manager", EditorGUI.indentLevel, EditorStyles.whiteLabel);
-            WindowScaleSettingsGUI.Draw(Settings.collectionsManagerWindowScale);
-            GUILayout.Space(1);
-            Helpers.IndentedLabel("Settings", EditorGUI.indentLevel, EditorStyles.whiteLabel);
-            WindowScaleSettingsGUI.Draw(Settings.settingsWindowScale);
-            EditorGUI.indentLevel--;
-            GUILayout.Space(4);
-            Helpers.DrawLine(Color.gray);
-            GUILayout.Space(4);
+            Helpers.IndentBlock(1, () =>
+            {
+                Settings.globalMinWindowScale = EditorGUILayout.Vector2Field("Global min", Settings.globalMinWindowScale);
+                // Only min needs to be clamped to a floor as max floor is already clamped by min.
+                Settings.globalMinWindowScale = Vector2.Max(Settings.globalMinWindowScale, Vector2.one);
+                Settings.globalMaxWindowScale = EditorGUILayout.Vector2Field("Global max", Settings.globalMaxWindowScale);
+                Settings.globalMaxWindowScale = Vector2.Max(Settings.globalMaxWindowScale, Settings.globalMinWindowScale);
+                GUILayout.Space(2);
+                Helpers.IndentedLabel("Palette", 0, EditorStyles.whiteLabel);
+                WindowScaleSettingsGUI.Draw(Settings.paletteWindowScale);
+                GUILayout.Space(1);
+                Helpers.IndentedLabel("Collections Manager", 0, EditorStyles.whiteLabel);
+                WindowScaleSettingsGUI.Draw(Settings.collectionsManagerWindowScale);
+                GUILayout.Space(1);
+                Helpers.IndentedLabel("Settings", 0, EditorStyles.whiteLabel);
+                WindowScaleSettingsGUI.Draw(Settings.settingsWindowScale);
+            });
+
+            Helpers.Line(Color.gray);
+
+            // Links
+            GUILayout.Label("Links:", EditorStyles.whiteLargeLabel);
+            Helpers.IndentBlock(1, () => 
+            { 
+                Helpers.LinkLabel("GitHub", "https://github.com/FrayedFunction/Prefab-Palette");
+                Helpers.LinkLabel("ReadMe", "https://github.com/FrayedFunction/Prefab-Palette/blob/main/Docs/README.md");
+                Helpers.LinkLabel("Support", "https://github.com/FrayedFunction/Prefab-Palette/issues");
+                Helpers.LinkLabel("License", "https://github.com/FrayedFunction/Prefab-Palette/blob/main/Docs/License.md");
+                Helpers.LinkLabel("Contribute", "https://github.com/FrayedFunction/Prefab-Palette/blob/update/global-settings-window/Docs/ContributionsGuide.md");
+                Helpers.LinkLabel("Donate", "https://ko-fi.com/frayedfunction");
+            });
+            Helpers.Line(Color.grey);
+
+            // Contact
+            GUILayout.Label("Contact:", EditorStyles.whiteLargeLabel);
+            Helpers.IndentBlock(1, () =>
+            {
+                EditorGUILayout.SelectableLabel(
+                    "reach@frayedfunction.com",
+                    GUILayout.Height(EditorGUIUtility.singleLineHeight)
+                );
+            });
+
+            Helpers.Line(Color.gray);
 
             GUILayout.Space(10);
             GUILayout.EndScrollView();
@@ -101,5 +127,6 @@ namespace PrefabPalette
             selected.value = EditorGUILayout.MaskField(label, selected.value, layerNames);
             return selected;
         }
+
     }
 }
