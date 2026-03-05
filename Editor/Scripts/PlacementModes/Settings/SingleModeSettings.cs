9a2d4c73674818eff51afba317c151212537a7ce
Frayed Function <65793243+FrayedFunction@users.noreply.github.com>
2026-03-05 14:21:49 +0000

    Remove range attribute

--- Editor/Scripts/PlacementModes/Settings/SingleModeSettings.cs
+++ Editor/Scripts/PlacementModes/Settings/SingleModeSettings.cs
@@ -6,7 +6,6 @@ namespace PrefabPalette
     {
         public Vector3 freeMode_placementOffset = Vector3.zero;
         public float freeMode_rotationSpeed = 2f;
-        [Range(0, 2)]
         public int selectedRotationAxis = 0;
     }
 }
